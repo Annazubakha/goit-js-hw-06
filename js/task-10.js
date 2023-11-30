22db08db1f30872ce5847d61bfe1c000ed314ce5
Annazubakha <korepovaanna@gmail.com>
2023-11-30 14:27:35 +0100

    Update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,19 +1,3 @@
-// Напиши скрипт створення і очищення колекції елементів.Користувач
-// вводить кількість елементів в input і натискає кнопку Створити,
-// після чого рендериться колекція.
-// // Натисненням на кнопку Очистити, колекція елементів очищається.
-//   // Створи функцію createBoxes(amount), яка приймає один параметр -
-//   число.Функція створює стільки < div >, скільки вказано в amount і
-//   додає їх у div#boxes.
-// // 1.	Розміри найпершого <div> - 30px на 30px.
-// // 2.	Кожен елемент після першого повинен бути ширшим і вищим
-// від попереднього на 10px.
-// // 3.	Всі елементи повинні мати випадковий колір фону у форматі HEX.
-// Використовуй готову функцію getRandomHexColor для отримання кольору.
-
-// // Створи функцію destroyBoxes(), яка очищає вміст div#boxes,
-// у такий спосіб видаляючи всі створені елементи.
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
@@ -25,14 +9,23 @@ const boxes = document.querySelector("#boxes");
 const input = document.querySelector("input");
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
+let size = 20;
 
 createBtn.addEventListener("click", oncreateBtnClick);
 destroyBtn.addEventListener("click", ondestroyBtnBtnClick);
 
-function oncreateBtnClick(input) {}
+function oncreateBtnClick() {
+  console.log(input.value);
+  for (let i = 0; i < input.value; i++) {
+    size += 10;
+    const item = `<div style="width: ${size}px; height: ${size}px;
+    background-color: ${getRandomHexColor()}; margin:2px"></div>`;
+    boxes.insertAdjacentHTML("beforeend", item);
+  }
+  input.value = "";
+}
 
-function ondestroyBtnBtnClick(event) {}
-
-function createBoxes(amount) {}
-
-function destroyBoxes() {}
+function ondestroyBtnBtnClick() {
+  boxes.innerHTML = "";
+  size = 20;
+}
